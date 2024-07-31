4138c92f07709eb9c51df7f103e6224a0baa63cb
Ripchic <theripper25@mail.ru>
2024-07-31 16:23:04 +0300

    device management buttons rem

--- handlers/device_management.py
+++ handlers/device_management.py
@@ -23,48 +23,13 @@ async def show_devices(callback_query: CallbackQuery, state: FSMContext, db: MDB
     await callback_query.message.edit_text(
         f"Ваши устройства: {len(user_devices)} шт. Выберите взаимодействия:",
         reply_markup=builders.inline_builder(
-            [*devices_names, "Добавить устройство ", "Удалить устройство ", "<< Назад", "Инструкция"],
+            [*devices_names, "<< Назад", "Инструкция"],
             [f"device_{i}" for i, _ in enumerate(user_devices)] +
-            ["add_device", "remove_device", "menu", "add_instruction"],
+            ["menu", "add_instruction"],
             [1 for _ in range(len(user_devices))] + [2, 2]
         )
     )
     await callback_query.answer()
-
-
-@router.callback_query(F.data == "remove_device")
-async def remove_device(callback_query: CallbackQuery, state: FSMContext):
-    await callback_query.message.edit_text("Введите имя устройства для удаления:", reply_markup=builders.inline_builder(
-        ["<< Назад"],
-        ["devices"],
-        [1]
-    ))
-    await state.set_state(DeviceManagement.remove_name)
-    await callback_query.answer()
-
-
-@router.message(DeviceManagement.remove_name)
-async def remove_device_name(message: Message, state: FSMContext, db: MDB):
-    device_name = message.text
-    user_id = message.from_user.id
-    user = await db.users.find_one({"_id": user_id})
-    user_devices = user["devices"]
-    if device_name not in user_devices:
-        await message.answer(
-            f"Устройство с именем '{device_name}' не найдено.\nПроверьте имя вашего устройства или повторите ввод.",
-            reply_markup=builders.inline_builder(
-                ["Мои устройства"],
-                ["devices"],
-                [1]
-            ))
-    else:
-        db.users.update_one({"_id": user_id}, {"$pull": {"devices": device_name}})
-        await state.clear()
-        await message.answer("Устройство успешно удалено", reply_markup=builders.inline_builder(
-            ["Мои устройства"],
-            ["devices"],
-            [1]
-        ))
 
 
 @router.callback_query(F.data == "add_instruction")
